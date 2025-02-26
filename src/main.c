cacc5a679ff3d53c34f5957317ee5efef93d4cc3
jerryrw <jerry@jerryrw.com>
2025-02-26 11:27:30 -0500

    minor changes

--- src/main.c
+++ src/main.c
@@ -8,6 +8,25 @@ int main(int argc, char *argv[])
     printf("Hello World Coffin");
 }
 
+// Example usage change hash size to 16 for MD5
+// int main()
+// {
+//     const char *filepath = "example.txt";
+//     uint8_t hash[32]; // 256 bits = 32 bytes change this to 128 bits 16 bytes for MD5
+
+//     if (calculate_file_sha256(filepath, hash) == 0)
+//     {
+//         printf("SHA-256 Hash: ");
+//         for (int i = 0; i < 32; ++i) // change this 32 to 16 for MD5
+//         {
+//             printf("%02x", hash[i]);
+//         }
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
+
 // save this snippet for later
 
 // void print_usage (char* argv[]) {
